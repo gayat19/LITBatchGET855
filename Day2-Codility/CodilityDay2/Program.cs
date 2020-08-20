0da5e2812e98a5e74926a66ecbd38bdfedfd647e
unknown <gayat19>
2020-08-20 18:55:16 +0530

    Day2 Codility

--- Day2-Codility/CodilityDay2/Program.cs
+++ Day2-Codility/CodilityDay2/Program.cs
@@ -10,18 +10,143 @@ namespace CodilityDay2
     {
         static void Main(string[] args)
         {
-            List<PlayerScore> players = new List<PlayerScore>
-            {
-                new PlayerScore(5,30),
-                new PlayerScore(4,20),
-                new PlayerScore(1,30),
-                new PlayerScore(2,10)
-            };
-            new Program().UnderstandingOrderOfPlusM();
+            //List<PlayerScore> players = new List<PlayerScore>
+            //{
+            //    new PlayerScore(5,30),
+            //    new PlayerScore(4,20),
+            //    new PlayerScore(1,30),
+            //    new PlayerScore(2,10)
+            //};
+            //new Program().UnderstandingOrderOfPlusM();
+            //int[] myArr = { 0, 0, 4, 2, 4, 5 };
+            //int[] res = new Program().CountElements(myArr);
+            //foreach (var item in res)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            int[] A = {4,1,1,2,1,2 };
+            int[] B = { 6,2,3,4};
+            bool res = new Program().CheckSwapArray(A, B, 10);
+            Console.WriteLine(res);
             Console.ReadKey();
         }
-        //O(1) or o(n log n) or O(n) or O(n+m)
-        private void UnderstandingOrderOfPlusM()
+        int[] CountElements(int[] arr)
+        {
+            //List<int> result = new List<int>();
+            int max = arr.Max();
+            int[] result = new int[max+1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int idx = arr[i];
+                result[idx]++;
+            }
+            return result;
+        }
+         bool CheckSwapArray(int[] A,int[] B,int m)
+        {
+            bool result = false;
+            int sumA = A.Sum();
+            int sumB = B.Sum();
+            Console.WriteLine(sumA+" "+sumB);
+            int diff = sumA - sumB;
+            if (diff % 2 != 0)
+                return result;
+            Array.Sort(A);
+            Array.Sort(B);
+            diff = diff / 2;
+            int i = 0, j = 0;
+            while(i<A.Length&&j<B.Length)
+            {
+                Console.WriteLine(A[i] + " " + B[j]+" "+i+" "+j);
+                int arrDiff = A[i] - B[j];
+                Console.WriteLine("Diff "+arrDiff+" "+diff);
+                if (arrDiff == diff)
+                {
+                    Console.WriteLine(A[i] + " " + B[j]);
+                    result = true;
+                    break;
+                }
+                else if (arrDiff < diff)
+                    i++;
+                else
+                    j++;
+                  
+            }
+            return result;
+        }
+
+        bool AnotherSwapCheck(int[] A, int[] B, int m)
+        {
+            bool result = false;
+            int sumA = A.Sum();
+            int sumB = B.Sum();
+            Console.WriteLine(sumA + " " + sumB);
+            int select = 1;
+            HashSet<int> nums = new HashSet<int>();
+            if(sumA > sumB)
+            {
+                for (int i = 0; i < B.Length; i++)
+                {
+                    nums.Add(B[i]);
+                }
+                select = 2;
+            }
+            else
+            {
+                for (int i = 0; i < A.Length; i++)
+                {
+                    nums.Add(A[i]);
+                }
+            }
+            int diff = (sumA - sumB)/2;
+            if (select == 1)
+            {
+                for (int i = 0; i < A.Length; i++)
+                {
+                    if (nums.Contains(A[i] + diff))
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < B.Length; i++)
+                {
+                    if (nums.Contains(B[i] + diff))
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        bool SwapAnotherCodility(int[] A, int[] B, int m)
+        {
+            bool result = false;
+            int sumA = A.Sum();
+            int sumB = B.Sum();
+            Console.WriteLine(sumA + " " + sumB);
+            int diff = sumA - sumB;
+            if (diff % 2 != 0)
+                return result;
+            diff = diff / 2;
+            int[] count = CountElements(A);
+            for (int i = 0; i < A.Length; i++)
+            {
+                if(0<=B[i]-diff && B[i]-diff < m && count[B[i]-diff]>0)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+            //O(1) or o(n log n) or O(n) or O(n+m)
+            private void UnderstandingOrderOfPlusM()
         {
             int num = 10;
             int sum = 0, prod = 1;
