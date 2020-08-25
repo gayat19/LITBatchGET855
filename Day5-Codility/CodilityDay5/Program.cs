3c847fe25fb7d04c0ad7c379343c79212c8f3990
unknown <gayat19>
2020-08-25 19:05:43 +0530

    Day5- Codility

---
+++ Day5-Codility/CodilityDay5/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityDay5
+{
+    class Program
+    {
+        void UnderstandingStack()
+        {
+            Stack<int> numStack = new Stack<int>();
+            numStack.Push(30);
+            numStack.Push(40);
+            numStack.Push(46);
+            numStack.Push(25);
+            numStack.Push(90);
+            foreach (var item in numStack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Element poped "+ numStack.Pop());
+            Console.WriteLine("Element peeked " + numStack.Peek());
+            Console.WriteLine("The number of elements in the stack now "+numStack.Count);
+        }
+        void UnderstandingQueue()
+        {
+            Queue<string> strQueue = new Queue<string>();
+            strQueue.Enqueue("Ramu");
+            strQueue.Enqueue("Bimu");
+            strQueue.Enqueue("Komu");
+            strQueue.Enqueue("Somu");
+            strQueue.Enqueue("Lomu");
+            foreach (var item in strQueue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Element removed from queue " + strQueue.Dequeue());
+            Console.WriteLine("Element peeked " + strQueue.Peek());
+            Console.WriteLine("The number of elements in the Queue now " + strQueue.Count);
+        }
+        public int solution(string S)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            char[] chararr = S.ToCharArray();
+            Stack<char> stk = new Stack<char>();
+            for (int i = 0; i < S.Length; i++)
+            {
+                if ('{' == S[i] || '(' == S[i] || '[' == S[i])
+                {
+                    stk.Push(S[i]);
+                }
+                else
+                {
+                    string pair = (Convert.ToString(stk.Pop()) + Convert.ToString(S[i]));
+                    if (pair != "{}" || pair != "[]" || pair != "()")
+                    {
+                        return 0;
+                    }
+                }
+            }
+            if (stk.Count>0)
+            {
+                return 0;
+            }
+
+            return 1;
+        }
+        static void Main(string[] args)
+        {
+            int i = new Program().solution("{[()()]}");
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
