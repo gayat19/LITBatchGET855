a805550aae23ec8228968d71f9da9b807fa91025
unknown <gayat19>
2020-09-08 18:44:52 +0530

    Day1-DevOps

---
+++ Day1-DevOps/SimpleSolution/SimpleSampleProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSampleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome "+name);
+            Console.ReadKey();
+        }
+    }
+}
